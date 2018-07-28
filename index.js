28f2253f3c38ced58afa8137cb277a7ee49b5747
Tom MacWright <tom@macwright.org>
2018-07-27 18:04:02 -0700

    Faster, globs

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
+#!/usr/bin/env node
+
+const argv = require("minimist")(process.argv.slice(2));
+const glob = require("glob");
 const fs = require("fs");
 const purify = require("purify-css");
 const cheerio = require("cheerio");
 const minify = require("html-minifier").minify;
 
-const htmlSource = fs.readFileSync(process.argv[2], "utf8");
-const $ = cheerio.load(htmlSource);
-const styles = $("style");
+glob.sync(argv._[0]).forEach((file, i, files) => {
+  const htmlSource = fs.readFileSync(file, "utf8");
+  const $ = cheerio.load(htmlSource);
 
-const css = styles
-  .map(function(s) {
-    return $(this).html();
-  })
-  .get()
-  .join("\n");
+  const styles = $("style");
 
-styles.each(function(index, element) {
-  if (index > 0) {
-    $(this).remove();
-  } else {
-    $(this).html("");
-  }
-});
+  const css = styles
+    .map(function(s) {
+      return $(this).html();
+    })
+    .get()
+    .join("\n");
 
-const purified = purify($.html(), css, { minify: true });
+  styles.each(function(index, element) {
+    if (index > 0) {
+      $(this).remove();
+    } else {
+      $(this).html("");
+    }
+  });
 
-$(styles.get(0)).html(purified);
+  const purified = purify($.html(), css, { minify: true });
 
-console.log(
-  minify($.html(), {
+  $(styles.get(0)).html(purified);
+
+  const output = minify($.html(), {
     collapseWhitespace: true,
     removeAttributeQuotes: true,
     sortAttributes: true,
     collapseBooleanAttributes: true,
     decodeEntities: true
-  })
-);
+  });
+
+  console.log(
+    `${`${i + 1}/${files.length}`.padStart(
+      String(files.length).length * 2 + 1,
+      " "
+    )} ${file}`
+  );
+  fs.writeFileSync(file, output);
+});
